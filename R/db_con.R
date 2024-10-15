73c6fcfe4481e78d26135234d00a449807a353db
SteveViss <steve.vissault@insileco.io>
2024-10-14 23:38:49 -0400

    Add local arg in init_db

--- R/db_con.R
+++ R/db_con.R
@@ -6,8 +6,12 @@
 #' }
 #' 
 #' @export
-init_con <- function(){
-    db <- file.path(db_path(), db_name())
+init_con <- function(local = NULL){
+    if(!is.null(local)){
+        db <- file.path(local)
+    } else (
+        db <- file.path(db_path(), db_name())
+    )
     stopifnot(db |> file.exists())
     return(DBI::dbConnect(RSQLite::SQLite(), db))
 }
